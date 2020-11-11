d3b4031f006fe1d5e14475bf68f2a49132f4da27
Cleve <mauricio.cleveland@gmail.com>
2020-11-11 09:31:48 -0300

    Added multiple instances

--- app/afn.py
+++ app/afn.py
@@ -8,12 +8,22 @@ def main():
     constants = Constants()
     reader = Reader('samples/dj38.tsp', constants)
     base_elements = reader.read_tsp()
-    for i in range(500):
+    minimal = None
+    min_elements = None
+    for i in range(1000):
         star = Star(base_elements, constants)
         star.life()
         elements = Helper.fision(star.elements)
         distance_matrix = reader.build_distance_matrix()
-        print(elements, ':', int(Helper.path_size(elements, distance_matrix)))
+        temporal = int(Helper.path_size(elements, distance_matrix))
+        if minimal is None:
+            minimal = temporal
+            min_elements = elements
+        elif temporal < minimal:
+            minimal = temporal
+            min_elements = elements
+
+    print(min_elements, ':', minimal)
 
 
 if __name__ == "__main__":
